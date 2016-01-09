98f95039fa7df76329008ec99d5517f7c1bdfef7
李浩 <lihao-mac@177-68-2-222.dsl.telesp.net.br>
2016-01-09 16:03:16 +0800

    Initial Commit

---
+++ LHDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHDBDemo
+//
+//  Created by 李浩 on 16/1/9.
+//  Copyright © 2016年 李浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHDBDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHDBDemo
+//
+//  Created by 李浩 on 16/1/9.
+//  Copyright © 2016年 李浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
